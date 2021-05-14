dea91be9e42bf758a14639cea8f1c3f85cdfbcfa
Your Name <miguelangel.gonzalez.rz@gmail.com>
2021-05-13 22:31:16 -0400

    Primer parte del codigo

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace Anagrama
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string FirstWord, SecondWord;
+            bool result;
+
+            Console.WriteLine("Escriba la primera palabra");
+            FirstWord = Console.ReadLine();
+            Console.WriteLine("Escriba la segunda palabra");
+            SecondWord = Console.ReadLine();
+
+            FirstWord.ToLower();
+            SecondWord.ToLower();
+
+            char[] FirstWord = FirstWord.ToCharArray();
+           
+
+
+
         }
     }
 }
